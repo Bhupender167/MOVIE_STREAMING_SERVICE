70b33d0c0650a64afec3df98776e6cecf0d024f2
Bhupender167 <vanshubaghel1@gmail.com>
2026-01-17 02:50:07 -0800

    Update api-client.ts

--- Desktop/sdk/MOVIE/src/services/api-client.ts
+++ Desktop/sdk/MOVIE/src/services/api-client.ts
@@ -1,6 +1,11 @@
+// src/services/api-client.ts
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
+
+if (!API_URL) {
+  throw new Error("VITE_API_URL is not defined");
+}
 
 const apiClient = axios.create({
   baseURL: API_URL,
